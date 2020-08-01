bd8a99372ed91071cd9c7d55c757181d577c7ee6
aruki <Joeld1027@gmail.com>
2020-08-01 15:49:44 -0500

    first commit

---
+++ script.test.js
@@ -0,0 +1,20 @@
+const googleSearch = require('./script');
+
+dbMock = [
+	'dog.com',
+	'cheesepufs.com',
+	'disney.com',
+	'dogpictures.com',
+];
+
+it('is a silly test', () => {
+	expect('hello').toBe('hello');
+});
+
+it('is searching google', () => {
+	expect(googleSearch('testtest', dbMock)).toEqual([]);
+	expect(googleSearch('dog', dbMock)).toEqual([
+		'dog.com',
+		'dogpictures.com',
+	]);
+});
